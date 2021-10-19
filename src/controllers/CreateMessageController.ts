2164aa3b5a85a5c9629ea480014eabfa19943ab5
ThalesTrombim <thalestr@outlook.com>
2021-10-18 22:02:04 -0300

    first commit

---
+++ src/controllers/CreateMessageController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+class CreateMessageController {
+    async handle(req: Request, res: Response){
+        const { message } = req.body;
+        
+
+    }
+}
+
+export { CreateMessageController }; 
